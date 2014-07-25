ba95fcdee832def8e5ea3c860b3c1b87a177f5d7
liye <liye@huaqin.com>
2014-07-25 09:44:34 +0800

    初步完成图片获取，待优化

--- src/com/huaqin/app/hqfilemanager/ButtonSwitchFragment.java
+++ src/com/huaqin/app/hqfilemanager/ButtonSwitchFragment.java
@@ -1,88 +1,159 @@
 package com.huaqin.app.hqfilemanager;
 
+
+import java.util.ArrayList;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.graphics.Color;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
-
-import com.huaqin.app.hqfilemanager.model.DummyGenerator;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ButtonSwitchFragment extends Fragment {
+	private final String IMAGE_TYPE = "image/*";
 	
-	public static final String TAG = ButtonSwitchFragment.class.getSimpleName();
+	ArrayList<Bitmap> listItems = new ArrayList<Bitmap>();
+	Cursor cursor;
+	private static final String PROJECTION[] = { "_data", };
+	private LayoutInflater mInflater;
+	private static final int UPDATE_ADAPTER = 1;
+	private GridAdapter simoleadapter;
 
-	private ItemListFragment fragLeft;
-	private ItemListFragment fragCenter;
-	private EmptyFragment fragRight;
-	
-	private View view;
-	private int indexFragment;
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		fragLeft = new ItemListFragment(DummyGenerator.getItemNumberList(), true);
-		fragCenter = new ItemListFragment(DummyGenerator.getItemAlphabetList(), true);
-		fragRight = new EmptyFragment(Color.WHITE);
-		
-		view = null;
-		indexFragment = 0;
+		Intent getAlbum = new Intent(Intent.ACTION_GET_CONTENT);
+		getAlbum.setType(IMAGE_TYPE);
+		Uri originalUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+		cursor = getActivity().getContentResolver().query(originalUri,// 指定缩略图数据库的Uri
+				PROJECTION,// 指定所要查询的字段
+				null,// 查询条件
+				null, // 查询条件中问号对应的值
+				null);
+		mInflater = LayoutInflater.from(getActivity());
 	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
-		view = inflater.inflate(R.layout.buttonswitch_fragment, container, false);
-		
-		RadioGroup group = (RadioGroup)view.findViewById(R.id.rb_group);
-		group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-			public void onCheckedChanged(RadioGroup group, int checkedId) {
-				//changeFragment(checkedId);
-				switch (group.getCheckedRadioButtonId()) {
-				case R.id.rb_left: changeFragment(0); break;
-				case R.id.rb_center: changeFragment(1); break;
-				case R.id.rb_right: changeFragment(2); break;
-				}
+
+	Handler handler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case UPDATE_ADAPTER:
+				simoleadapter.notifyDataSetChanged();
+				break;
 			}
-		});
-		
-		changeFragment(indexFragment);
-		
-		return view;
-	}
-	
-	public void changeFragment(int index) {
-		FragmentManager fragMgr = getFragmentManager();
-		FragmentTransaction fragTrs = fragMgr.beginTransaction();
-		
-		indexFragment = index;
-		switch (indexFragment) {
-		case 0:
-			fragTrs.replace(R.id.lyt_main_fragment_container, new ItemListFragment(DummyGenerator.getItemNumberList(), true));
-			break;
-		case 1:
-			fragTrs.replace(R.id.lyt_main_fragment_container, new ItemListFragment(DummyGenerator.getItemAlphabetList(), true));
-			break;
-		case 2:
-			fragTrs.replace(R.id.lyt_main_fragment_container, new EmptyFragment(Color.WHITE));
-			break;
-		/*case 0:
-			fragTrs.replace(R.id.lyt_main_fragment_container, fragLeft);
-			break;
-		case 1:
-			fragTrs.replace(R.id.lyt_main_fragment_container, fragCenter);
-			break;
-		case 2:
-			fragTrs.replace(R.id.lyt_main_fragment_container, fragRight);
-			break;*/
+			super.handleMessage(msg);
 		}
-		fragTrs.commit();
+	};
+	Runnable updateThread = new Runnable() {
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			cursor.moveToFirst();
+			while (cursor.moveToNext()) {
+
+				String path = cursor.getString(cursor
+						.getColumnIndex(MediaStore.Images.Thumbnails.DATA));
+				Bitmap imageBitmap = getBitmap(path);
+				listItems.add(imageBitmap);
+				handler.sendEmptyMessage(UPDATE_ADAPTER);
+			}
+
+		}
+
+		private Bitmap getBitmap(String path) {
+			return convertToBitmap(path);
+		}
+
+	};
+
+	public Bitmap convertToBitmap(String path) {
+
+		BitmapFactory.Options opts = new BitmapFactory.Options();
+		// 设置为ture只获取图片大小
+		opts.inJustDecodeBounds = true;
+		opts.inPreferredConfig = Bitmap.Config.ARGB_4444;
+		// 返回为空
+		BitmapFactory.decodeFile(path, opts);
+		opts.inJustDecodeBounds = false;
+		opts.inSampleSize = (int) 8f;
+		Bitmap bitmap = BitmapFactory.decodeFile(path, opts);
+		getActivity().getCacheDir();
+		return bitmap;
 	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootview = inflater.inflate(R.layout.buttonswitch_fragment,
+				container, false);
+		simoleadapter = new GridAdapter();
+		grid = (GridView) rootview.findViewById(R.id.GridView_picture);
+		grid.setAdapter(simoleadapter);
+		new Thread(updateThread).start();
+		return rootview;
+
+	}
+
+	class GridAdapter extends BaseAdapter {
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = mInflater.inflate(R.layout.image, null);
+				holder = new ViewHolder();
+				holder.image = (ImageView) convertView
+						.findViewById(R.id.image_Item);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			holder.image.setImageBitmap(listItems.get(position));
+			return convertView;
+		}
+
+		// set the imageView using the path of image
+		public void setViewImage(ImageView v, String value) {
+			try {
+				Bitmap bitmap = BitmapFactory.decodeFile(value);
+				Bitmap newBit = Bitmap
+						.createScaledBitmap(bitmap, 100, 80, true);
+				v.setImageBitmap(newBit);
+			} catch (NumberFormatException nfe) {
+				v.setImageURI(Uri.parse(value));
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return listItems != null ? listItems.size() : 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return listItems != null ? listItems.get(position) : null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+	}
+
+	static class ViewHolder {
+		public static ImageView image;
+	}
+	GridView grid;
 
 }
